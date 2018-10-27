2a648af2403f1199bdfba1c855d3a7b5fce4124d
pi <pi@pi>
2018-10-27 15:52:37 -0500

    Update instal RF24 script should work

--- installRF24.sh
+++ installRF24.sh
@@ -2,21 +2,25 @@
 
 echo "Make sure to enable SPIDEV first"
 
-sudo apt update
-sudo apt upgrade
+#sudo apt update
+#sudo apt upgrade
 
 cd ~
 mkdir -p ece445
-
+cd ~/ece445
+pwd
 # Download BCM2835 library
 wget http://www.airspayce.com/mikem/bcm2835/bcm2835-1.57.tar.gz
-tar xvf bcm2835-1.57
+tar xvf bcm2835-1.57.tar.gz
 
 # Remove zip file
 rm bcm2835-1.57.tar.gz
 
-cd bcm2835-1.57.tar.gz
-sudo make install -B
+cd bcm2835-1.57
+./configure
+make
+sudo make check
+sudo make install
 cd ..
 
 # Building RF24 using SPIDEV
@@ -28,16 +32,17 @@ cd ..
 
 # Install RF24Network
 wget https://github.com/TMRh20/RF24Network/archive/Development.zip
-unzip RF24Network-Development.zip
-rm RF24Network-Development.zip
+unzip Development.zip
+rm Development.zip
 cd RF24Network-Development
 sudo make install
 cd ..
 
 # Install RF24Mesh
-wget https://github.com/TMRh20/RF24Mesh/archive/master.zip
-unzip RF24Mesh-master.zip
-rm RF24Mesh-master.zip
+#wget https://github.com/TMRh20/RF24Mesh/archive/master.zip
+wget -O master.zip https://github.com/nRF24/RF24Mesh/archive/v1.0.51.zip
+unzip master.zip
+rm master.zip
 cd RF24Mesh-master
 sudo make install
 cd ..
