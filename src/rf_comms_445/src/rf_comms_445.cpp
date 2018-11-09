67fc112e99dd6048f988ce78c82421776c9e0696
FunkyNoodles <nickasds@hotmail.com>
2018-11-08 20:24:47 -0600

    Added RF payload message

--- src/rf_comms_445/src/rf_comms_445.cpp
+++ src/rf_comms_445/src/rf_comms_445.cpp
@@ -1,14 +1,30 @@
+// ROS
 #include <ros/ros.h>
-#include <std_msgs/Int64.h>
+#include <std_msgs/String.h>
+#include <rf_comms_445/RFPayload.h>
+
+// RF24
 #include <RF24/RF24.h>
 #include <RF24Network/RF24Network.h>
 #include <RF24Mesh/RF24Mesh.h>
+
+// STL
 #include <iostream>
 #include <ctime>
 #include <stdio.h>
 #include <time.h>
 #include <fstream>
 #include <string>
+
+struct payload {
+	char messageType;
+	unsigned int seqNum;
+	float x;
+	float y;
+	float orientation;
+	bool taskDone;
+};
+
 
 // Setup for GPIO 22 CE and CE1 CSN with SPI Speed @ 8Mhz
 RF24 radio(RPI_V2_GPIO_P1_15, BCM2835_SPI_CS0, BCM2835_SPI_SPEED_8MHZ);  
@@ -17,6 +33,11 @@ RF24Network network(radio);
 RF24Mesh mesh(radio,network);
 
 bool amMaster = false;
+std::string rfMessageToSend;
+
+void rfSendCallback(const rf_comms_445::RFPayload::ConstPtr & msg) {
+	//rfMessageToSend = msg->data;
+}
 
 int main(int argc, char **argv) {
 	// get node ID from file
@@ -42,7 +63,8 @@ int main(int argc, char **argv) {
 
 	ros::NodeHandle n;
 
-	ros::Publisher chatter_pub = n.advertise<std_msgs::Int64>("rf_received", 1000);
+	ros::Publisher rfPub = n.advertise<rf_comms_445::RFPayload>("rf_received", 1000);
+	ros::Subscriber rfSub = n.subscribe("rf_to_send", 1000, rfSendCallback);
 
 	ros::Rate loop_rate(200);
 
@@ -52,7 +74,9 @@ int main(int argc, char **argv) {
 		if(amMaster){
 			mesh.DHCP();
 		}
-
+		while (network.available()){
+			RF24NetworkHeader header;
+		}
 
 		ros::spinOnce();
 
